332af7af1f00fce5e7ae81ab9e377889fccd7262
itsuki-kawamura-dev <kawamura.itsuki.work@gmail.com>
2026-09-24 00:00:35 +0100

    call test bedrock

--- lambda/app.py
+++ lambda/app.py
@@ -1,9 +1,34 @@
 import json
+import boto3
+
+bedrock = boto3.client("bedrock-runtime")
+
+MODEL_ID = "amazon.nova-micro-v1:0"
 
 
 def lambda_handler(event, context):
     body = json.loads(event.get("body", "{}"))
     question = body.get("question", "")
+
+    response = bedrock.converse(
+        modelId=MODEL_ID,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "text": question
+                    }
+                ]
+            }
+        ],
+        inferenceConfig={
+            "maxTokens": 300,
+            "temperature": 0.3
+        }
+    )
+
+    answer = response["output"]["message"]["content"][0]["text"]
 
     return {
         "statusCode": 200,
@@ -11,6 +36,6 @@ def lambda_handler(event, context):
             "Content-Type": "application/json"
         },
         "body": json.dumps({
-            "question_received": question
+            "answer": answer
         })
     }
--- terraform/iam.tf
+++ terraform/iam.tf
@@ -19,3 +19,23 @@ resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
   role       = aws_iam_role.lambda.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+resource "aws_iam_role_policy" "lambda_bedrock" {
+  name = "${var.project_name}-bedrock-policy"
+  role = aws_iam_role.lambda.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Action = [
+          "bedrock:InvokeModel"
+        ]
+
+        Resource = "*"
+      }
+    ]
+  })
+}
